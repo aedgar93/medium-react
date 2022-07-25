f33e8ebc47b4d7b7ae564a5f29e672668b948f3a
aeherr <anna.edgar.herr@gmail.com>
2022-07-25 13:39:07 -0700

    Updates

---
+++ precache-manifest.bffa66725696e1855ddf87a811930ee7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebda1deff20cd2edde00119f458f9a9d",
+    "url": "/medium-react-new/index.html"
+  },
+  {
+    "revision": "8768eba2a15fcfc26d1c",
+    "url": "/medium-react-new/static/css/2.ee77217e.chunk.css"
+  },
+  {
+    "revision": "efd1f6f7bc416bb1cadf",
+    "url": "/medium-react-new/static/css/main.77a2f549.chunk.css"
+  },
+  {
+    "revision": "8768eba2a15fcfc26d1c",
+    "url": "/medium-react-new/static/js/2.f63a9e8b.chunk.js"
+  },
+  {
+    "revision": "b17af3a2a2049a794a10230ada17eae8",
+    "url": "/medium-react-new/static/js/2.f63a9e8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd1f6f7bc416bb1cadf",
+    "url": "/medium-react-new/static/js/main.471d3629.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/medium-react-new/static/js/main.471d3629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff60fb279d44882afdd",
+    "url": "/medium-react-new/static/js/runtime-main.e3ae1b48.js"
+  },
+  {
+    "revision": "e2df770e530e9aad05882cdb60228322",
+    "url": "/medium-react-new/static/media/logo.e2df770e.svg"
+  }
+]);
